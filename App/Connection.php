4fd0225def4feac3758a0bb33fd81ac33841445e
Adson Jesus <adsonjesus2014@gmail.com>
2021-08-13 01:54:59 -0300

    Alterando db

--- App/Connection.php
+++ App/Connection.php
@@ -8,9 +8,9 @@ class Connection {
 		try {
 
 			$conn = new \PDO(
-				"mysql:host=localhost;dbname=twitter_clone;charset=utf8",
-				"root",
-				"" 
+				"mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_cbfe855288e3785;charset=utf8",
+				"b3178c598f1128",
+				"788a3376" 
 			);
 
 			return $conn;
